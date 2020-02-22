a205a1422af521f2683d1cf6b6e112b5ab9c2899
Ata Türkoğlu <ataturkoglu2003@gmail.com>
2020-02-22 16:55:21 +0300

    Linear & Radial Progress Bars

---
+++ Assets/UI Lib/Scripts/ProgressBar.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UILib
+{
+
+#if UNITY_EDITOR
+    using UnityEditor;
+#endif
+
+    [ExecuteInEditMode]
+    public class ProgressBar : MonoBehaviour
+    {
+        #region Unity Editor
+#if UNITY_EDITOR
+        [MenuItem("GameObject/UI/UI Lib/Linear Progress Bar")]
+        public static void AddLinearProgressBar()
+        {
+            GameObject obj = Instantiate(Resources.Load<GameObject>("Linear Progress Bar"));
+            obj.transform.SetParent(Selection.activeGameObject.transform, false);
+        }
+
+        [MenuItem("GameObject/UI/UI Lib/Radial Progress Bar")]
+        public static void AddRadialProgressBar()
+        {
+            GameObject obj = Instantiate(Resources.Load<GameObject>("Radial Progress Bar"));
+            obj.transform.SetParent(Selection.activeGameObject.transform, false);
+        }
+#endif
+        #endregion
+
+        public float min;
+        public float max = 1;
+        public float current = 0;
+        public Color fillColor = Color.red;
+        public bool isRadial;
+        [Range(0,1)] public float innerRadius;
+        public Sprite innerCircleIcon;
+        public Color innerCircleColor;
+
+        [SerializeField] private Image mask;
+        [SerializeField] private Image filler;
+        [SerializeField] private Image innerCircle;
+
+        private void Update()
+        {
+            GetCurrentFill();
+            if (isRadial)
+                UpdateInnerCircle();
+        }
+
+        private void GetCurrentFill()
+        {
+            float currentOffset = current - min;
+            float maxOffset = max - min;
+
+            float fillAmount = currentOffset / maxOffset;
+            if (mask)
+                mask.fillAmount = fillAmount;
+
+            filler.color = fillColor;
+        }
+
+        private void UpdateInnerCircle()
+        {
+            innerCircle.GetComponent<RectTransform>().localScale = Vector2.one * innerRadius;
+            innerCircle.transform.Find("Icon").GetComponent<Image>().sprite = innerCircleIcon;
+            innerCircle.color = innerCircleColor;
+        }
+    }
+}
---
+++ Assets/UI Lib/Scripts/ProgressBarCustomEditor.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEditor.Callbacks;
+
+
+namespace UILib
+{
+    [CustomEditor(typeof(ProgressBar))]
+    public class ProgressBarCustomEditor : Editor
+    {
+        SerializedProperty min;
+        SerializedProperty max;
+        SerializedProperty current;
+        SerializedProperty fillColor;
+        SerializedProperty isRadial;
+        SerializedProperty innerRadius;
+        SerializedProperty innerCircleIcon;
+        SerializedProperty innerCircleColor;
+
+        SerializedProperty mask;
+        SerializedProperty filler;
+        SerializedProperty innerCircle;
+
+        void OnEnable()
+        {
+            min = serializedObject.FindProperty("min");
+            max = serializedObject.FindProperty("max");
+            current = serializedObject.FindProperty("current");
+            fillColor = serializedObject.FindProperty("fillColor");
+            isRadial = serializedObject.FindProperty("isRadial");
+            innerRadius = serializedObject.FindProperty("innerRadius");
+            innerCircleIcon = serializedObject.FindProperty("innerCircleIcon");
+            innerCircleColor = serializedObject.FindProperty("innerCircleColor");
+
+            mask = serializedObject.FindProperty("mask");
+            filler = serializedObject.FindProperty("filler");
+            innerCircle = serializedObject.FindProperty("innerCircle");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+
+            EditorGUILayout.Slider(max, 0, 1000, new GUIContent("Max Value"));
+            EditorGUILayout.Slider(min,0 , max.floatValue, new GUIContent("Minimum Value"));
+            EditorGUILayout.Slider(current, 0 , max.floatValue, new GUIContent("Current Value"));
+            fillColor.colorValue = EditorGUILayout.ColorField("Fill Color", fillColor.colorValue);
+            
+            EditorGUILayout.Separator();
+
+            EditorGUILayout.PropertyField(mask, new GUIContent("Mask"));
+            EditorGUILayout.PropertyField(filler, new GUIContent("Filler"));
+
+            EditorGUILayout.Separator();
+
+            EditorGUILayout.PropertyField(isRadial, new GUIContent("Is Radial"));
+            if (isRadial.boolValue)
+            {
+                EditorGUILayout.PropertyField(innerRadius, new GUIContent("Inner Radius"));
+                EditorGUILayout.PropertyField(innerCircleIcon, new GUIContent("Inner Circle Icon"));
+                EditorGUILayout.PropertyField(innerCircle, new GUIContent("Inner Circle"));
+                innerCircleColor.colorValue = EditorGUILayout.ColorField("Inner Circle Color", innerCircleColor.colorValue);
+            }
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
